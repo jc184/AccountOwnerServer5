8eca6937df58298ae7450e4d0745a2de6d0b11a0
james chalmers <james.chalmers184@gmail.com>
2021-08-01 13:11:02 +0100

    Add project files.

---
+++ AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Owner, OwnerDto>();
+        CreateMap<Account, AccountDto>();
+        CreateMap<OwnerForCreationDto, Owner>();
+        CreateMap<OwnerForUpdateDto, Owner>();
+    }
+}
---
+++ Contracts/IOwnerRepository .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Models;
+
+namespace Contracts
+{
+    public interface IOwnerRepository : IRepositoryBase<Owner>
+    {
+        IEnumerable<Owner> GetAllOwners();
+        Owner GetOwnerById(Guid ownerId);
+        Owner GetOwnerWithDetails(Guid ownerId);
+        void CreateOwner(Owner owner);
+        void UpdateOwner(Owner owner);
+        void DeleteOwner(Owner owner);
+    }
+}
